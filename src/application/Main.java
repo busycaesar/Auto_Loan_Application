6299fc587e5dcda52fd76833c5ea8fb4c07c9306
Dev Shah <busycaesar@gmail.com>
2024-02-23 18:55:24 -0500

    Refactor(View): Updated dimensions of the application!

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,9 +12,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,815,515);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			// Setting the property of the stage!
+			primaryStage.setTitle("AutoLoan");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
