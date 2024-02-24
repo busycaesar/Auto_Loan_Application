81cbf86fb3e8ad5463ea05f5b68b6dfc4d273783
Dev Shah <busycaesar@gmail.com>
2024-02-24 10:43:22 -0500

    Chore(Main): Add comments to explain the start function of the application.

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,23 +8,31 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	// Override the start function of the application.
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			// Create a BorderPane and load the fxml file.
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
+			
+			// Create a scene, load the BorderPane, and set the application dimensions.
 			Scene scene = new Scene(root,815,515);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// Set the scene on the stage.
 			primaryStage.setScene(scene);
-			// Setting the property of the stage!
+			
+			// Set the property of the stage.
 			primaryStage.setTitle("AutoLoan");
 			primaryStage.setResizable(false);
+			
+			// Display the stage.
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		} catch(Exception e) { e.printStackTrace(); }
 	}
 	
+	// Main function of the program.
 	public static void main(String[] args) {
+		// Launching the application.
 		launch(args);
 	}
 }
