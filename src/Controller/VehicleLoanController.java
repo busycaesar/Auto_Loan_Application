83569d38ca6ca13ab7f209ba0abd3be0ee1e8257
Dev Shah <busycaesar@gmail.com>
2024-02-26 14:36:56 -0500

    Feat(Controller): Add function for calculating loan amount

--- src/Controller/VehicleLoanController.java
+++ src/Controller/VehicleLoanController.java
@@ -7,11 +7,17 @@ public class VehicleLoanController {
 	private Vehicle vehicle;
 	private Finance finance;
 	
-	public VehicleLoanController(String _type, String _age, double _price, double _downPayment, double _interest, double _paymentAmount, int _durationInYears, String _frequency) {
-		
+	public VehicleLoanController(String _type, String _age, double _price, double _downPayment, double _interest, int _durationInMonths, String _frequency) {
+
+		double _paymentAmount = this.calculatePaymentAmount();
 		this.vehicle = new Vehicle(_type, _age, _price);
-		this.finance = new Finance(this.vehicle, _downPayment, _interest, _paymentAmount, _durationInYears, _frequency);
+		this.finance = new Finance(this.vehicle, _price - _downPayment, _interest, _paymentAmount, _durationInMonths, _frequency);
 		
+	}
+	
+	private double calculatePaymentAmount() {
+		// Calculate the loan amount according to 
+		return 0.0;
 	}
 	
 }
