a5044ec90fca7048aafa2021bf6dbf13593687ca
Dev Shah <busycaesar@gmail.com>
2024-02-26 14:03:33 -0500

    Add(Finance Class): Declare variable to store loan price

--- src/Model/Finance.java
+++ src/Model/Finance.java
@@ -4,16 +4,18 @@ public class Finance {
 
 	private Vehicle item; 		     // Item for the loan. In our case the item is Vehicle type.
 	private double  downPayment,
-				    interest,
+					loanAmount,
+				    interestRate,
 				    paymentAmount;   // Amount to be paid by the customer at the stored interval(i.e. frequency)
 	private int		durationInYears;
 	private String  frequency;       // Frequency of the loan: Monthly, Weekly, Bi-weekly.
 	
-	public Finance(Vehicle _item, double _downPayment, double _interest, double _paymentAmount, int _durationInYears, String _frequency) {
+	public Finance(Vehicle _item, double _downPayment, double _interestRate, double _paymentAmount, int _durationInYears, String _frequency) {
 		
 		this.item = _item;
 		this.downPayment = _downPayment;
-		this.interest = _interest;
+		this.loanAmount = this.item.getPrice() - this.downPayment;
+		this.interestRate = _interestRate;
 		this.paymentAmount = _paymentAmount; 
 		this.durationInYears =_durationInYears;
 		this.frequency =_frequency;
