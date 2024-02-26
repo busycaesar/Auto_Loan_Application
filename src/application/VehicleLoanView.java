8f3c19f9f0596bce2fa7d52ab9e5f703018e03e1
Dev Shah <busycaesar@gmail.com>
2024-02-26 14:36:22 -0500

    Add(ViewController): Add toggleGroup from fxml controller

--- src/application/VehicleLoanView.java
+++ src/application/VehicleLoanView.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class VehicleLoanView {
 
@@ -11,6 +12,8 @@ public class VehicleLoanView {
 	private ChoiceBox<String> vehicleType, loanPaymentFrequency;
 	@FXML
 	private RadioButton newVehicleAge, oldVehicleAge;
+	@FXML
+	private ToggleGroup vehicleAge;
 	@FXML
 	private TextField vehiclePrice, vehicleDownpayment, loanInterestRate;
 	
@@ -25,6 +28,9 @@ public class VehicleLoanView {
 		
 		this.vehicleType.getItems().addAll("Car", "Truct", "Family Van");
 		this.loanPaymentFrequency.getItems().addAll("Monthly", "Bi-weekly", "Weekly");
+		this.vehicleAge = new ToggleGroup();
+		this.newVehicleAge.setToggleGroup(this.vehicleAge);
+		this.oldVehicleAge.setToggleGroup(this.vehicleAge);
 		
 	}
 	
@@ -46,6 +52,20 @@ public class VehicleLoanView {
 
 	@FXML
 	public void calculateLoan() {
+		String vehicleType  		= this.vehicleType.getValue(),
+			   vehicleAge   		= ((RadioButton)this.vehicleAge.getSelectedToggle()).getText(),
+			   loanPaymentFrequency = this.loanPaymentFrequency.getValue();
+		
+		try {
+			
+			double vehiclePrice 	  = Double.parseDouble(this.vehiclePrice.getText()),
+				   vehicleDownPayment = Double.parseDouble(this.vehicleDownpayment.getText()),
+				   loanInterestRate	  = Double.parseDouble(this.loanInterestRate.getText());
+			
+		}
+		catch (NumberFormatException e){ 
+			System.out.println(e.getMessage());
+		}
 		
 	}
 	
